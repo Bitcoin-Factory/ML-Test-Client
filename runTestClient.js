39f71c1d2586a312897c0d5b9ae28793b09ff845
Luis-Fernando-Molina <Luis-Fernando-Molina@users.noreply.github.com>
2022-03-15 12:34:10 +0100

    Update runTestClient.js

--- runTestClient.js
+++ runTestClient.js
@@ -2,5 +2,5 @@ const TEST_CLIENT_MODULE = require('./TestClient')
 const testClient = TEST_CLIENT_MODULE.newMachineLearningTestClient()
 
 testClient.initialize()
-setTimeout (testClient.run, 5000)
+testClient.run()
 
